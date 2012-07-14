abcd443f8d10c69f236fc0560d3397d738f6f754
Baglan Dosmagambetov <baglan.dos@gmail.com>
2012-07-14 22:05:55 +0600

    Initial Commit

---
+++ MCSoundBoard/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MCSoundBoard
+//
+//  Created by Baglan Dosmagambetov on 7/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MCSoundBoard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCSoundBoard
+//
+//  Created by Baglan Dosmagambetov on 7/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
